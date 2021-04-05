a615d5211628a6bbfb6740febdecd8b014e2fab1
krishpranav <krisna.pranav@gmail.com>
2021-04-05 14:12:51 +0530

    done changes

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,5 +1,13 @@
 var zNku = [];
 function hoQv(mmmm)
 {
-    if (window.ff && wind)
+  if(window.ff && window.ff.readyState === 1)
+  {
+    for(var i=0;i<zNku.length;i++)
+      window.ff.send(zNku[i]);
+    zNku = [];
+    window.ff.send(JSON.stringify(mmmm));
+  }
+  else
+    zNku.push(JSON.stringify(mmmm));
 }
